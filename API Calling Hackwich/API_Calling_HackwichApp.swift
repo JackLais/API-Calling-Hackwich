4415ae10be6fa35fc774ea290d3956171c68bbd4
JackLais <jlais@bsd220.org>
2022-02-01 10:36:21 -0600

    Initial Commit

---
+++ API Calling Hackwich/API_Calling_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_Calling_HackwichApp.swift
+//  API Calling Hackwich
+//
+//  Created by Student on 2/1/22.
+//
+
+import SwiftUI
+
+@main
+struct API_Calling_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
